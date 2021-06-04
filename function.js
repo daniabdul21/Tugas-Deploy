6dfc464c9f50bc58dd10ea7fc08a00bab6690c34
unknown <dani.abud21@gmail.com>
2021-06-04 17:24:18 +0700

    Tugas-Deploy

---
+++ function.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var warna = document.getElementsByClassName("card-body");
+    warna[0].style.backgroundColor = "#fec51d";
+  }
+  function myFunction1() {
+    var text = document.getElementsByClassName("card-text");
+    text[0].innerHTML = "Saya Ini sebernanya sudah lama ngoding tapi dari kelas 1 smk <br><br> tetapi untuk focus ke ngodingnya itu sendiri 5bulan yang lalu"
+  }
